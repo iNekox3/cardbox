ecf9f3bcc7b858b0ba1c0ba650ffa666345fd374
iNekox3 <inekox3@hotmail.com>
2021-09-05 16:05:08 +0800

    feat: format data to have no attribute types

--- infra/index.js
+++ infra/index.js
@@ -1,18 +1,19 @@
 const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
 const dynamodb = new DynamoDBClient({ region: process.env.AWS_REGION });
 
 async function handler() {
   const params = {
     TableName: process.env.AWS_DYNAMODB_TABLE_NAME,
-    Key: {
-      [process.env.AWS_DYNAMODB_PARTITION_KEY]: { S: process.env.TEST_USER },
-    },
+    Key: marshall({
+      [process.env.AWS_DYNAMODB_PARTITION_KEY]: process.env.TEST_USER,
+    }),
   };
 
   try {
     const data = await dynamodb.send(new GetItemCommand(params));
-    console.log("Success", data);
+    console.log("Success", unmarshall(data.Item));
   } catch (err) {
     console.log("Error", err);
   }
